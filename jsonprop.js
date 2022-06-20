31504aa0ccf2f2d27442549776dc415a50d7b573
Mugesh Kumar K <105528383+Mugesh2297@users.noreply.github.com>
2022-06-20 13:18:01 +0530

    Add files via upload

---
+++ jsonprop.js
@@ -0,0 +1,11 @@
+var obj1 = { name: "Person 1", age:5 };
+
+
+var obj2 = { name: "Person 1", age:5 };
+// Using JavaScript
+JSON.stringify(obj1) === JSON.stringify(obj2); // false
+
+// Using Lodash
+//_.isEqual(obj1, obj2);
+
+//we can use lodash to compare the values rather then using JSON
